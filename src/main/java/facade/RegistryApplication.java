9e00e53895876918c5d9b5be8c47474296bb4bbc
Ajay Koranne <akoranne@gmail.com>
2017-02-02 12:00:41 -0600

    correction

--- src/main/java/facade/RegistryApplication.java
+++ src/main/java/facade/RegistryApplication.java
@@ -3,15 +3,14 @@ package facade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableAutoConfiguration
 public class RegistryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryApplication.class, args);
 	}
 }
+
